3727add8190c83c6bf0c7f36bf5627e08bf44953
Junichi Hayashi <j-hayasi@ist.osaka-u.ac.jp>
2018-12-07 03:22:10 +0900

    Use latest bash-completion

--- etc/init/init.sh
+++ etc/init/init.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+HOME_USR_SHARE=$HOME/usr/share
+[ ! -e $HOME_USR_SHARE ] && mkdir -p $HOME_USR_SHARE
+
+# Download latest bash-completion
+BASH_COMP_DIR=$HOME_USR_SHARE/bash-completion
+BASH_COMP_SH=$BASH_COMP_DIR/bash-completion
+if [ ! -f $BASH_COMP_SH ]; then
+    [ ! -e $BASH_COMP_DIR ] && mkdir $BASH_COMP_DIR
+    wget -O $BASH_COMP_SH https://salsa.debian.org/debian/bash-completion/raw/master/bash_completion
+    chmod u+x $BASH_COMP_SH
+fi
